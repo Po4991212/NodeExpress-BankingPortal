2d01b2bffa105be3a534e8fe4c99f50de6ca1c34
Po4991212 <vinhphucvb@gmail.com>
2022-04-12 23:16:01 -0500

    read account data

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = JSON.parse(
+  fs.readFileSync("./json/accounts.json", { encoding: "utf8", flag: "r" })
+);
 app.get("/", (req, res) => res.render("index", { title: "Index" }));
 
 app.listen(3000, () => console.log("PS Project run on port 3000"));
