1b489132c6eae7b79df75c706a128ea4c93ec157
Po4991212 <vinhphucvb@gmail.com>
2022-04-12 23:11:29 -0500

    adding index.ejs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const { view } = require("ramda");
+const res = require("express/lib/response");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.render("index", { title: "Index" }));
+
+app.listen(3000, () => console.log("PS Project run on port 3000"));
