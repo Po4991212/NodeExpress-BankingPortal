d0feaa4f93a4e5c308a89a52688aa00366d80500
Po4991212 <vinhphucvb@gmail.com>
2022-04-13 00:18:18 -0500

    module2

--- src/app.js
+++ src/app.js
@@ -14,7 +14,42 @@ const accountData = fs.readFileSync("./src/json/accounts.json", {
   encoding: "utf8",
   flag: "r",
 });
+const userData = fs.readFileSync("./src/json/users.json", {
+  encoding: "utf8",
+  flag: "r",
+});
 const accounts = JSON.parse(accountData);
-app.get("/", (req, res) => res.render("index", { title: "Index" }));
+const users = JSON.parse(userData);
+
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Account Summary",
+    accounts: accounts,
+  })
+);
+
+app.get("/savings", (req, res) =>
+  res.render("account", {
+    account: accounts.savings,
+  })
+);
+
+app.get("/checking", (req, res) =>
+  res.render("account", {
+    account: accounts.checking,
+  })
+);
+
+app.get("/credit", (req, res) =>
+  res.render("account", {
+    account: accounts.credit,
+  })
+);
+
+app.get("/profile", (req, res) =>
+  res.render("profile", {
+    user: users[0],
+  })
+);
 
 app.listen(3000, () => console.log("PS Project run on port 3000"));
